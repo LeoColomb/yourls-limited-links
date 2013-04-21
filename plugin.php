33c7dc3f62f8b28e4e3a3dbbf61a36c0d975a78b
Léo Colombaro <leocolombaro@yahoo.fr>
2013-04-21 17:55:10 +0300

    Tabify

--- plugin.php
+++ plugin.php
@@ -17,31 +17,30 @@ define( 'LIMIT_BEFORE_CHAR', '_' );
 // do something, if yourls doesn't recognise a valid short link
 yourls_add_action( 'loader_failed', 'lpc_limit_request' );
 
-function lpc_limit_request($args) 
-{
-        $request = $args[0];
-        $pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
-        if( preg_match( "@^([$pattern]+)".LIMIT_BEFORE_CHAR."[0-9]$@", $request, $matches ) ) {
-                $keyword = isset( $matches[1] ) ? $matches[1] : '';
-                $keyword = yourls_sanitize_keyword( $keyword );
-				$number = substr($request, strrpos($request, LIMIT_BEFORE_CHAR) + 1);
-                if( yourls_is_shorturl( $keyword ) ) {
-					lpc_do_or_not_redirect( $keyword, $number );
-					die();
-				}
-        }
+function lpc_limit_request($args) {
+	$request = $args[0];
+	$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
+	if( preg_match( "@^([$pattern]+)".LIMIT_BEFORE_CHAR."[0-9]$@", $request, $matches ) ) {
+		$keyword = isset( $matches[1] ) ? $matches[1] : '';
+		$keyword = yourls_sanitize_keyword( $keyword );
+		$number = substr($request, strrpos($request, LIMIT_BEFORE_CHAR) + 1);
+		if( yourls_is_shorturl( $keyword ) ) {
+			lpc_do_or_not_redirect( $keyword, $number );
+			die();
+		}
+	}
 }
 
-function lpc_do_or_not_redirect( $keyword, $number ){
-		$totalClicks = yourls_get_keyword_clicks( $keyword );
-		if ( $number <= $totalClicks ){
-			yourls_redirect( YOURLS_SITE, 301 );
-		} else {
-			// Update click count in main table
-			$update_clicks = yourls_update_clicks( $keyword );
-			// Update detailed log for stats
-			$log_redirect = yourls_log_redirect( $keyword );
-			$url = yourls_get_keyword_longurl( $keyword );
-			yourls_redirect( $url, 301 );	
-		}
+function lpc_do_or_not_redirect( $keyword, $number ) {
+	$totalClicks = yourls_get_keyword_clicks( $keyword );
+	if ( $number <= $totalClicks ){
+		yourls_redirect( YOURLS_SITE, 301 );
+	} else {
+		// Update click count in main table
+		$update_clicks = yourls_update_clicks( $keyword );
+		// Update detailed log for stats
+		$log_redirect = yourls_log_redirect( $keyword );
+		$url = yourls_get_keyword_longurl( $keyword );
+		yourls_redirect( $url, 301 );	
+	}
 }
